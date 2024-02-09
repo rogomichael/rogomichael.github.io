fae2d3397f9f8e5c040f2bf85351c7f223c04ea8
rogomichael <aloonyokinda@gmail.com>
2024-02-09 18:17:05 +0100

    test website

---
+++ js/summer_school.js
@@ -0,0 +1,13 @@
+function showMoreSpeakers()
+  {
+    const speakers = document.querySelector("#speakers");
+    speakers.className = "speakers_grid_open";
+
+     //if (speakers.className == "speakers_grid_open") {
+       // read less
+      // speakers.className = "speakers_grid_hidden";
+    // } else {
+       //read more
+      // speakers.className = "speakers_grid_open";
+    // }
+  }
